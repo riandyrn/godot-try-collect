481cef25f08adaa0ebaf77f10e7967bc154c80b8
Riandy Rahman Nugraha <riandyrn@gmail.com>
2020-04-21 13:36:20 +0300

    using touch to control player movement; add android export preset;

--- Player.gd
+++ Player.gd
@@ -6,8 +6,15 @@ var velocity: Vector2
 var screen_size: Vector2
 var is_stop: bool
 
+var target: Vector2
+
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
+
 func _ready():
 	screen_size = get_viewport().size
+	target = position
 
 # Hoo.., ok, jadi nilai dari delta ini lebih kecil 
 # daripada 1 ya? Makanya dikalikan delta untuk speednya
@@ -16,16 +23,20 @@ func _process(delta):
 	velocity = Vector2()
 	
 	if not is_stop:
-		# determine the velocity change based on user input
-		if Input.is_action_pressed("ui_left"):
-			velocity.x -= 1
-		if Input.is_action_pressed("ui_right"):
-			velocity.x += 1
-		if Input.is_action_pressed("ui_down"):
-			velocity.y += 1
-		if Input.is_action_pressed("ui_up"):
-			velocity.y -= 1
-		
+#		# determine the velocity change based on user input
+#		if Input.is_action_pressed("ui_left"):
+#			velocity.x -= 1
+#		if Input.is_action_pressed("ui_right"):
+#			velocity.x += 1
+#		if Input.is_action_pressed("ui_down"):
+#			velocity.y += 1
+#		if Input.is_action_pressed("ui_up"):
+#			velocity.y -= 1
+		if position.distance_to(target) > 10:
+			velocity = target-position
+		else:
+			velocity = Vector2()
+	
 	if velocity.length() > 0:
 		$AnimatedSprite.play()
 		$AnimatedSprite.flip_h = (velocity.x < 0)
